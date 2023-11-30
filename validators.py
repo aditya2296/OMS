d231114ccd4c277c033a93aa8bf804d928849881
amodi <aditya.modi@netcracker.com>
2023-11-30 14:33:40 +0530

    ^Customer page with SQLAlchemy database

--- validators.py
+++ validators.py
@@ -1,22 +1,4 @@
-import secrets, sqlite3
+import secrets
 
 def generate_secret_key():
     return secrets.token_hex(16)
-
-def create_customers_table():
-    conn = sqlite3.connect('customer_details_table.db')
-    c = conn.cursor()
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS customer_details_table (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            customername TEXT NOT NULL,
-            customerlocation TEXT NOT NULL,
-            customerphonenumber INTEGER NOT NULL,
-            customeremail TEXT NOT NULL,
-            customerbillingtype TEXT NOT NULL,
-            customerdeliverytype TEXT NOT NULL,
-            customerroute TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
